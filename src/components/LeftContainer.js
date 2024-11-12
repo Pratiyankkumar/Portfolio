7a3cb379fbe96fbbe384a60e54d42ec2bf2773ef
pratiyankkumar <kumarpratiyank@gmail.com>
2024-11-12 19:02:23 +0530

    Added a scroll effecr

--- src/components/LeftContainer.js
+++ src/components/LeftContainer.js
@@ -5,27 +5,39 @@ import { Projects } from "./Projects";
 import { About } from "./About";
 import { Contact } from "./Contact";
 import { CodeHighlights } from "./CodeHighlights";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export function LeftContainer() {
+  // Initialize AOS
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // controls animation duration (1000ms here)
+      easing: "ease-in-out", // easing for smoothness
+      once: false, // only animate once per scroll
+    });
+  }, []);
+
   return (
     <div className="sm:w-4/5 w-full bg-dark font-sans flex flex-col items-start ml-10 sm:ml-36 md:ml-72 justify-center">
-      {/* About section - add mt-10 class */}
-      <div className="mt-10 about one" id="about">
+      <div data-aos="fade-up" className="mt-10 about one" id="about">
         <About />
       </div>
 
-      {/* Tech Stack section - add mt-6 class */}
-      <div className="mt-6 tech-stack two" id="tech-stack">
+      <div data-aos="fade-up" className="mt-6 tech-stack two" id="tech-stack">
         <TechStack />
       </div>
 
-      {/* Projects section - add mt-5 class */}
-      <div className="mt-5 projects three" id="projects">
+      <div data-aos="fade-up" className="mt-5 projects three" id="projects">
         <Projects />
       </div>
 
-      {/* Code Highlights section - add mt-6 class */}
-      <div className="mt-6 code-highlights four" id="code-highlights">
+      <div
+        data-aos="fade-up"
+        className="mt-6 code-highlights four"
+        id="code-highlights"
+      >
         <CodeHighlights name="1. Auth Middleware" codeSnippet={authCodeSnippet}>
           <Heading
             upperText="CODE"
@@ -39,8 +51,7 @@ export function LeftContainer() {
         />
       </div>
 
-      {/* Contact section - add mt-6 class */}
-      <div className="mt-6 contact five" id="contact">
+      <div data-aos="fade-up" className="mt-6 contact five" id="contact">
         <Contact />
       </div>
     </div>
