ad6b81e1aa9d410c73df25a5eaca5b65fe2082a8
Pratiyank Kumar <159152851+Pratiyankkumar@users.noreply.github.com>
2026-03-31 07:29:22 +0530

    fix

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -26,12 +26,6 @@ export const portfolio = {
       live: "https://npmjs.com/package/@pratiyank/create-express-app",
       github: "https://github.com/Pratiyankkumar/create-express-app",
     },
-    {
-      name: "AI Educational Video Generator",
-      description:
-        "A tool that generates educational videos from text prompts using AI.",
-      live: "https://testing-theta-eosin.vercel.app/",
-    },
   ],
   experience: [
     {
